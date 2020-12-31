fdffad24d35781e44f32605d3661ef612f8daa48
2-hill <jamil.rahou@laposte.net>
2020-12-31 22:10:19 +0100

    scss

---
+++ src/componants/WeeklyForecast.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class WeeklyForecast extends React.Component {
+    render() {
+        return (
+
+                <div className="WeeklyContainer"></div>  
+            
+       )   
+    }
+}
+
+export default WeeklyForecast;
