c32dd7b2b4d5edace312687bbece04e776784a00
2-hill <jamil.rahou@laposte.net>
2020-12-31 22:28:02 +0100

    css grid

--- src/componants/App.js
+++ src/componants/App.js
@@ -4,17 +4,11 @@ import Container from "./Container"
 class App extends React.Component {
     render() {
         return (
-            <div className="offset-md-2 container justify-content-md-center">
-                <div className="row">
-                    <div className="col-sm-2">
-                        <div className="jumbotron">
-                            
-                        </div>
-                    </div>
-                    <div className="col-sm-10">
-                        <Container />
-                    </div>
-                    </div>
+            <div class="grid-container">
+                
+                    
+                <Container/>
+
             </div>
         )
     }
