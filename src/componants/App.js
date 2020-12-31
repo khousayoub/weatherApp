63a50f72cd5747ab3755e9c2b19e4d7f166d9893
khousm <mohamed.khous@univ-avignon.fr>
2020-12-31 19:36:01 +0100

    initializing the application and the App componant

---
+++ src/componants/App.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Welcome to our Weather App !</div>
+        )
+    }
+}
+
+export default App;
