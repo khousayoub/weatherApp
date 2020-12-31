7f0404edeeccfe6b1d0d48c684e173675af37b78
khousm <mohamed.khous@univ-avignon.fr>
2020-12-31 21:22:35 +0100

    test

--- src/componants/City.js
+++ src/componants/City.js
@@ -5,7 +5,7 @@ class City extends React.Component {
         return (
             <div>
                 <img src="../images/london.jpg" className="img-thumbnail img-fluid rounded d-block" alt="London"/>
-                <h4 className="text-center">London</h4>  
+                <h4 className="text-center">London 1</h4>  
             </div>
         )
         
